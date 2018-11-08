3e3c85b3d57832acbcd7ce0cde9a7403356b49b4
Danny van Dam <d.van.dam@persgroep.nl>
2018-11-08 10:28:25 +0100

    Use 'manual' conversion instead of type classes

---
+++ src/main/scala/nl.dpes/recruiterResponse/convertor/package.scala
@@ -0,0 +1,36 @@
+package nl.dpes.recruiterResponse
+
+package object convertor {
+  implicit def convert(from: Address[Id]): Address[Option] = Address[Option](
+    Some(from.streetNameAndHouseNumber),
+    Some(from.city),
+    Some(from.zipCode)
+  )
+
+  implicit def convert(from: Person[Id]): Person[Option] = Person[Option](
+    Some(from.firstName),
+    Some(from.lastName),
+    Some(from.emailAddres),
+    Some(from.phoneNumber),
+    Some(convert(from.address))
+  )
+
+  def checkValueAvailable[A](value: Option[A], fieldName: String): Option[ValidationError[A]] = value match {
+    case None => Some(ValidationError(s"$fieldName is mandatory"))
+    case _ => None
+  }
+
+  implicit def convertOptionalAddress(from: Address[Option]): Either[Unit, Address[Id]] = from match {
+    case Address(Some(streetNameAndHouseNumber), Some(city), Some(zipCode)) =>
+      Right(Address[Id](streetNameAndHouseNumber, city, zipCode))
+    case Address(streetNameAndHouseNumber, city, zipCode) =>
+      Left(())
+  }
+
+  implicit def convertOptionalPerson(from: Person[Option]): Either[Unit, Person[Id]] = from match {
+    case Person(Some(firstName), Some(lastName), Some(emailAddres), Some(phoneNumber), Some(address)) => for {
+      address <- convertOptionalAddress(address)
+    } yield Person[Id](firstName, lastName, emailAddres, phoneNumber, address)
+    case _ => Left(())
+  }
+}
--- src/main/scala/nl.dpes/recruiterResponse/package.scala
+++ src/main/scala/nl.dpes/recruiterResponse/package.scala
@@ -1,7 +1,7 @@
 package nl.dpes
 
-import nl.dpes.recruiterResponse
-import nl.dpes.recruiterResponse.Convertor.RecruiterIdToOptionConvertor
+import nl.dpes.recruiterResponse._
+import nl.dpes.recruiterResponse.convertor._
 import nl.dpes.thrush._
 
 package object recruiterResponse {
@@ -18,13 +18,13 @@ package object recruiterResponse {
   type OptionalValidationError[T] = Option[ValidationError[T]]
 
   case class Address[C[_]](streetNameAndHouseNumber: C[String], city: C[String], zipCode: C[String])
-  case class Recruiter[C[_]](firstName: C[String],
-                             lastName: C[String],
-                             emailAddres: C[String],
-                             phoneNumber: C[String],
-                             address: C[Address[C]])
+  case class Person[C[_]](firstName: C[String],
+                          lastName: C[String],
+                          emailAddres: C[String],
+                          phoneNumber: C[String],
+                          address: C[Address[C]])
 
-  val errors: Recruiter[OptionalValidationError] = Recruiter[OptionalValidationError](
+  val errors: Person[OptionalValidationError] = Person[OptionalValidationError](
     firstName = Some(ValidationError("te lang", "includes last name")),
     lastName = None,
     emailAddres = None,
@@ -32,9 +32,9 @@ package object recruiterResponse {
     address = None
   )
 
-  val incompleteRecruiter: Recruiter[Option] = Recruiter[Option](Some("first name"), None, None, None, None)
+  val incompletePerson: Person[Option] = Person[Option](Some("first name"), None, None, None, None)
 
-  val recruiter: Recruiter[Id] = Recruiter[Id](
+  val person: Person[Id] = Person[Id](
     "hoi",
     "daar",
     "foo@bar.baz",
@@ -42,46 +42,13 @@ package object recruiterResponse {
     Address[Id]("mt.Lincolnweg", "Amsterdam", "1033SN")
   )
 
-
-  def checkValueAvailable[A](value: Option[A], fieldName: String): Option[ValidationError[A]] = value match {
-    case None => Some(ValidationError(s"$fieldName is mandatory"))
-    case _ => None
-  }
-
-  //  def toId(recruiter: Recruiter[Option]): Either[Recruiter[OptionalValidationError], Recruiter[Id]] = recruiter match {
-  //    case Recruiter(Some(firstName), Some(lastName), Some(emailAddres), Some(phoneNumber), Some(address)) =>
-  //      Right(Recruiter[Id](firstName, lastName, emailAddres, phoneNumber, address))
-  //    case Recruiter(firstName, lastName, emailAddres, phoneNumber, address) =>
-  //      Left(Recruiter[OptionalValidationError](
-  //        checkValueAvailable(firstName, "firstName"),
-  //        checkValueAvailable(lastName, "lastName"),
-  //        checkValueAvailable(emailAddres, "emailAddres"),
-  //        checkValueAvailable(phoneNumber, "phoneNumber"),
-  //        checkValueAvailable(address, "address")
-  //      ))
-  //  }
-
-  //  val validated: Either[Recruiter[OptionalValidationError], Recruiter[Id]] = toId(incompleteRecruiter)
-
-//  def idToOption[A[_], B](from: A[Id[B]])(implicit convertor: Convertor[A, Id, Option]): A[Option[B]] =
-//    implicitly[Convertor[A, Id, Option]].convert(from)
-
-
-  val completeRecruiter: Recruiter[Option] = RecruiterIdToOptionConvertor.convert(recruiter) //idToOption(recruiter)
+  val completePerson: Person[Option] = person
 }
 
 object Test extends App {
-//  def showResult(recruiter: Either[Recruiter[OptionalValidationError], Recruiter[Id]]): Unit = recruiter match {
-//    case Left(recruiterResponse.Recruiter(firstName, lastName, emailAddres, phoneNumber, address)) =>
-//      Seq("LEFT", firstName, lastName, emailAddres, phoneNumber, address) foreach println
-//
-//    case Right(recruiterResponse.Recruiter(firstName, lastName, emailAddres, phoneNumber, address)) =>
-//      Seq("RIGHT", firstName, lastName, emailAddres, phoneNumber, address) foreach println
-//  }
-//
-//  incompleteRecruiter |> toId |> showResult
-//
-//  completeRecruiter |> toId |> showResult
+  def showResult(person: Either[Unit, Person[Id]]): Unit = println(person)
 
-  println(recruiterResponse.completeRecruiter)
+  incompletePerson |> convertOptionalPerson |> showResult
+
+  completePerson |> convertOptionalPerson |> showResult
 }
