79d3f624fca56d1e66bb192efa1f789211df8f78
Danny van Dam <d.van.dam@persgroep.nl>
2018-11-21 14:40:33 +0100

    Cleanup client demo values as the tests cover all situations

--- src/main/scala/nl.dpes/cats_future_either/Client.scala
+++ src/main/scala/nl.dpes/cats_future_either/Client.scala
@@ -9,12 +9,7 @@ trait Client {
   def deductCredit(id: String): Future[Either[String, Unit]]
 }
 object Client extends Client {
-//  def getCredits: Future[Either[String, Seq[String]]] = Future.successful(Right(Seq("profit")))
-//  def getCredits: Future[Either[String, Seq[String]]] = Future.successful(\/-(Seq()))
-//  def getCredits: Future[Either[String, Seq[String]]] = Future.successful(-\/("get credits error"))
-  def getCredits: Future[Either[String, Seq[String]]] = Future.failed(new Exception("failed retrieving credits"))
+  def getCredits: Future[Either[String, Seq[String]]] = Future.successful(Right(Seq("profit")))
 
   def deductCredit(id: String): Future[Either[String, Unit]] = Future.successful(Right(()))
-//  def deductCredit(id: String): Future[Either[String, Unit]] = Future.successful(-\/("deduct credits error"))
-//  def deductCredit(id: String): Future[Either[String, Unit]] = Future.failed(new Exception("failed deducting credits"))
 }
